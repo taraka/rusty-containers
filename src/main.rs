62e55341866b09d163aff1e8001a8d7c1a62cd62
Tom Rawcliffe <thomas.rawcliffe@skybettingandgaming.com>
2020-02-07 17:06:59 +0000

    Extracting CString helpers

--- src/main.rs
+++ src/main.rs
@@ -15,17 +15,9 @@ fn main() {
 
 fn entry_point(program: &str, args: &[&str]) -> () {
 
-    let program_cstring = CString::new(program.as_bytes()).unwrap();
+    let program_cstring = cstring(program);
+    let arg_charptrs = cstring_array(args);
 
-    let arg_cstrings = args.into_iter().map(|&arg| {
-        CString::new(arg.as_bytes())
-    }).collect::<Result<Vec<_>, _>>().unwrap();
-
-    let mut arg_charptrs: Vec<_> = arg_cstrings.iter().map(|arg| {
-        arg.as_ptr()
-    }).collect();
-
-    arg_charptrs.push(ptr::null());
 
     unsafe {
         execv(program_cstring.as_ptr(), arg_charptrs.as_ptr());
@@ -35,4 +27,22 @@ fn entry_point(program: &str, args: &[&str]) -> () {
 fn do_clone() -> bool {
     let pid = unsafe { fork() };
     return pid != 0;
+}
+
+fn cstring(input: &str) -> CString {
+    CString::new(input.as_bytes()).unwrap()
+}
+
+fn cstring_array(input: &[&str]) -> Vec<*const i8> {
+    let arr_cstrings = input.into_iter().map(|&arg| {
+        CString::new(arg.as_bytes())
+    }).collect::<Result<Vec<_>, _>>().unwrap();
+
+    let mut arr_charptrs: Vec<_> = arr_cstrings.iter().map(|arg| {
+        arg.as_ptr()
+    }).collect();
+
+    arr_charptrs.push(ptr::null());
+
+    return arr_charptrs;
 }
