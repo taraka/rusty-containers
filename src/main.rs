d296edb0fab7fa9ac3e6e867a65a6f63f8a9c763
Tom <git@tomrawcliffe.co.uk>
2020-02-08 14:13:13 +0000

    Using clone and exec

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 use nix::unistd::execv;
-use nix::unistd::{fork, ForkResult};
+use nix::sched;
 use std::ffi::{CString, CStr};
 
 fn main() {
-    let is_child = do_clone();
-    if is_child {
-        entry_point("/bin/hostname", &["hostname"]);
-        return(); // Not needed as exec will holt execution but just here for clarity
-    }
+
+    do_clone();
 
     println!("I'm here in the parent process");
     return()
+}
+
+fn child() -> isize {
+    entry_point("/bin/hostname", &["hostname"]);
+    return 0;
 }
 
 fn entry_point(program: &str, args: &[&str]) -> () {
@@ -22,13 +24,18 @@ fn entry_point(program: &str, args: &[&str]) -> () {
     execv(program_cstring.as_c_str(), arg_charptrs.as_slice()).expect("Exec failed");
 }
 
-fn do_clone() -> bool {
-    return match fork().expect("Fork Failed") {
-        ForkResult::Child => true,
-        ForkResult::Parent { child: _ } => false
-    }
-
+fn do_clone() {
+    const STACK_SIZE: usize = 1024 * 1024;
+    let ref mut stack: [u8; STACK_SIZE] = [0; STACK_SIZE];
+    let cbk = Box::new(|| child());
+    let p = sched::clone(cbk, stack, sched::CloneFlags::CLONE_NEWUTS, None);
+    let _p = match p {
+        Ok(p) => p,
+        Err(err) => panic!(err),
+    };
 }
+
+
 
 fn cstring(input: &str) -> CString {
     CString::new(input.as_bytes()).unwrap()
