bc684fef5b70449a74a0566df1ce53854711b6ef
Tom <git@tomrawcliffe.co.uk>
2020-02-08 14:02:19 +0000

    Getting a working fork exec

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use nix::unistd::execv;
+use nix::unistd::{fork, ForkResult};
 use std::ffi::{CString, CStr};
-use std::ptr;
 
 fn main() {
-//    let is_child = do_clone();
-//    if is_child {
-        entry_point("/usr/bin/hostname", &["hostname"]);
+    let is_child = do_clone();
+    if is_child {
+        entry_point("/bin/hostname", &["hostname"]);
         return(); // Not needed as exec will holt execution but just here for clarity
-//    }
-//
-//    println!("I'm here in the parent process");
-//    return()
+    }
+
+    println!("I'm here in the parent process");
+    return()
 }
 
 fn entry_point(program: &str, args: &[&str]) -> () {
@@ -19,22 +19,23 @@ fn entry_point(program: &str, args: &[&str]) -> () {
     let arg_charptrs = cstring_array(args);
 
 
-    unsafe {
-        execv(program_cstring.as_c_str(), arg_charptrs.as_slice());
-    }
+    execv(program_cstring.as_c_str(), arg_charptrs.as_slice()).expect("Exec failed");
 }
 
-//fn do_clone() -> bool {
-//    let pid = unsafe { clone() };
-//    return pid != 0;
-//}
+fn do_clone() -> bool {
+    return match fork().expect("Fork Failed") {
+        ForkResult::Child => true,
+        ForkResult::Parent { child: _ } => false
+    }
+
+}
 
 fn cstring(input: &str) -> CString {
     CString::new(input.as_bytes()).unwrap()
 }
 
-fn cstring_array(input: &[&str]) -> Vec<&'static CStr> {
+fn cstring_array<'a>(input: &[&str]) -> Vec<&'a CStr> {
     input.into_iter().map(|&arg| {
-        CString::new(arg.as_bytes()).as_c_str()
+        Box::leak(Box::new(CString::new(arg).unwrap())).as_c_str()
     }).collect()
 }
