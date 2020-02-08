45794bdc3629a5e3796a16a3b5dd8d71c6e97d1b
Tom Rawcliffe <thomas.rawcliffe@skybettingandgaming.com>
2020-02-08 13:06:06 +0000

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use libc::{execv, fork};
-use std::ffi::CString;
+use nix::unistd::execv;
+use std::ffi::{CString, CStr};
 use std::ptr;
 
 fn main() {
-    let is_child = do_clone();
-    if is_child {
+//    let is_child = do_clone();
+//    if is_child {
         entry_point("/usr/bin/hostname", &["hostname"]);
         return(); // Not needed as exec will holt execution but just here for clarity
-    }
-
-    println!("I'm here in the parent process");
-    return()
+//    }
+//
+//    println!("I'm here in the parent process");
+//    return()
 }
 
 fn entry_point(program: &str, args: &[&str]) -> () {
@@ -20,29 +20,21 @@ fn entry_point(program: &str, args: &[&str]) -> () {
 
 
     unsafe {
-        execv(program_cstring.as_ptr(), arg_charptrs.as_ptr());
+        execv(program_cstring.as_c_str(), arg_charptrs.as_slice());
     }
 }
 
-fn do_clone() -> bool {
-    let pid = unsafe { fork() };
-    return pid != 0;
-}
+//fn do_clone() -> bool {
+//    let pid = unsafe { clone() };
+//    return pid != 0;
+//}
 
 fn cstring(input: &str) -> CString {
     CString::new(input.as_bytes()).unwrap()
 }
 
-fn cstring_array(input: &[&str]) -> Vec<*const i8> {
-    let arr_cstrings = input.into_iter().map(|&arg| {
-        CString::new(arg.as_bytes())
-    }).collect::<Result<Vec<_>, _>>().unwrap();
-
-    let mut arr_charptrs: Vec<_> = arr_cstrings.iter().map(|arg| {
-        arg.as_ptr()
-    }).collect();
-
-    arr_charptrs.push(ptr::null());
-
-    return arr_charptrs;
+fn cstring_array(input: &[&str]) -> Vec<&'static CStr> {
+    input.into_iter().map(|&arg| {
+        CString::new(arg.as_bytes()).as_c_str()
+    }).collect()
 }
