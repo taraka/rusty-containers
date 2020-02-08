78a9e038b70a6a8776fd8668ede0311acc4a4a24
Tom <git@tomrawcliffe.co.uk>
2020-02-08 21:51:45 +0000

    Chroot and proc mounted

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,60 @@
-use nix::unistd::{execv, gethostname, sethostname, sleep};
+use nix::unistd::{execv, gethostname, sethostname, getpid, getppid, chroot, mkdir, chdir, Pid};
 use nix::sched::{CloneFlags, clone};
+use nix::sys::stat;
+use nix::mount::{mount, MsFlags};
 use std::ffi::{CString, CStr};
 use std::str::from_utf8;
+use std::path::Path;
+use std::{time, thread};
+use nix::sys::wait::waitpid;
 
+const BASE_DIR: &str = "/home/tom/Code/rusty-containers/containers";
+static name: &str = "container1";
 
 fn main() {
     create_container();
 }
 
 fn create_container() {
-    do_clone();
+    let container_base = String::from(BASE_DIR) + "/" + name;
+    mkdir::<str>(&container_base[..], stat::Mode::S_IRWXU | stat::Mode::S_IRGRP | stat::Mode::S_IXGRP);
 
-    sleep(1);
+    let child = do_clone();
+    println!("Child PID: {}", child);
+    waitpid(child, None);
 
-    let ref mut buf: [u8; 256] = [0; 256];
-    gethostname(buf).expect("Getting hostname failed");
 
-    println!("I'm here in the parent process on : {}", from_utf8(buf).expect("Get hostname from string failed"));
+
+//    sleep(1);
+
+//    let ref mut buf: [u8; 256] = [0; 256];
+//    gethostname(buf).expect(-"Getting hostname failed");
+//
+//    println!("I'm here in the parent process on : {}", from_utf8(buf).expect("Get hostname from string failed"));
     return()
 }
 
 fn child() -> isize {
 
-    sethostname("newhostname").expect("Setting hostname failed");
-    entry_point("/bin/hostname", &["hostname"]);
+    //thread::sleep(time::Duration::from_secs(180));
+    let container_base = String::from(BASE_DIR) + "/" + name;
+
+    chroot(&container_base[..]);
+    chdir("/");
+
+    mount::<str, str,  str, str>(Some("proc"), "proc", Some("proc"), MsFlags::MS_REC ^ MsFlags::MS_REC, None).expect("Mount proc failed");
+
+    sethostname(name).expect("Setting hostname failed");
+
+    println!("Child PID: {}", getpid());
+    println!("Parent PID: {}", getppid());
+
+    entry_point("/bin/ls", &["ls", "-la", "/proc"]);
+
     return 0;
 }
 
-
-
-fn entry_point(program: &str, args: &[&str]) -> () {
-
-    let program_cstring = cstring(program);
-    let arg_charptrs = cstring_array(args);
-
-
-    execv(program_cstring.as_c_str(), arg_charptrs.as_slice()).expect("Exec failed");
-}
-
-fn do_clone() {
+fn do_clone() -> Pid {
     const STACK_SIZE: usize = 1024 * 1024;
     let ref mut stack: [u8; STACK_SIZE] = [0; STACK_SIZE];
     let cbk = Box::new(|| child());
@@ -52,14 +68,17 @@ fn do_clone() {
         CloneFlags::CLONE_NEWNET |
         CloneFlags::CLONE_NEWNS;
 
-    let p = clone(cbk, stack, flag_bits , None);
-    let _p = match p {
-        Ok(p) => p,
-        Err(err) => panic!(err),
-    };
+    clone(cbk, stack, flag_bits , None).expect("Clone Failed")
 }
 
+fn entry_point(program: &str, args: &[&str]) -> () {
 
+    let program_cstring = cstring(program);
+    let arg_charptrs = cstring_array(args);
+
+
+    execv(program_cstring.as_c_str(), arg_charptrs.as_slice()).expect("Exec failed");
+}
 
 fn cstring(input: &str) -> CString {
     CString::new(input.as_bytes()).unwrap()
