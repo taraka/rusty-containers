9ab7dcc62bd28db77fc03f0f963f4af1df08cfa2
Tom <git@tomrawcliffe.co.uk>
2020-02-08 15:43:52 +0000

    all name spaces are now created

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use nix::unistd::{execv, gethostname, sethostname, sleep};
-use nix::sched;
+use nix::sched::{CloneFlags, clone};
 use std::ffi::{CString, CStr};
 use std::str::from_utf8;
 
-fn main() {
 
+fn main() {
+    create_container();
+}
+
+fn create_container() {
     do_clone();
 
     sleep(1);
@@ -12,7 +16,7 @@ fn main() {
     let ref mut buf: [u8; 256] = [0; 256];
     gethostname(buf).expect("Getting hostname failed");
 
-    println!("I'm here in the parent process on : {}", from_utf8(buf).expect("Get hostname failed"));
+    println!("I'm here in the parent process on : {}", from_utf8(buf).expect("Get hostname from string failed"));
     return()
 }
 
@@ -22,6 +26,8 @@ fn child() -> isize {
     entry_point("/bin/hostname", &["hostname"]);
     return 0;
 }
+
+
 
 fn entry_point(program: &str, args: &[&str]) -> () {
 
@@ -36,7 +42,17 @@ fn do_clone() {
     const STACK_SIZE: usize = 1024 * 1024;
     let ref mut stack: [u8; STACK_SIZE] = [0; STACK_SIZE];
     let cbk = Box::new(|| child());
-    let p = sched::clone(cbk, stack, sched::CloneFlags::CLONE_NEWUTS, None);
+
+    let flag_bits: CloneFlags =
+        CloneFlags::CLONE_NEWCGROUP |
+        CloneFlags::CLONE_NEWUTS |
+        CloneFlags::CLONE_NEWIPC |
+        CloneFlags::CLONE_NEWUSER |
+        CloneFlags::CLONE_NEWPID |
+        CloneFlags::CLONE_NEWNET |
+        CloneFlags::CLONE_NEWNS;
+
+    let p = clone(cbk, stack, flag_bits , None);
     let _p = match p {
         Ok(p) => p,
         Err(err) => panic!(err),
